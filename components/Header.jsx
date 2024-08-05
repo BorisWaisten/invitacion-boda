6cf5c09f21cc326e9d95b57082dc3eec5c4058d6
BorisWaisten <boriswaisten@gmail.com>
2024-08-05 12:00:03 -0300

    .

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,14 +10,14 @@ const Header = () => {
           className="backgroundHeader m-1 rounded-lg"
         ></div>
         <div className="container text-center relative mt-10 mb-10  px-4">
-          <p className={`text-xl sm:text-2xl  mb-6`}>22.03.25</p>
+          <p className={`text-base sm:text-lg md:text-2xl font-bold  mb-6`}>22.03.25</p>
           <h1
             className="titleHeader font-extrabold mt-10"
             style={{ fontFamily: 'Asmelina Harley' }}
           >
             Martu y Gino
           </h1>
-          <div className={`text-lg sm:text-sm md:text-2xl  mt-10 `}>
+          <div className={`text-base sm:text-lg md:text-2xl lg:text-2xl mt-10 `}>
             <p>
               Ante Dios y nuestros seres queridos, nos uniremos en matrimonio.
               Acompáñanos en esta bendita celebración.
