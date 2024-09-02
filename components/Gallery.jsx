9f8e7bb5f2920752b94e01316d6ffc3d405980ee
BorisWaisten <boriswaisten@gmail.com>
2024-09-02 12:39:41 -0300

    touch tactil

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -9,12 +9,13 @@ import './components.css';
 import iconSvg from '../public/iconCamara.svg';
 import { cinzelDecorative } from '@/public/fonts';
 
-const images = [w1, w5, w3, w4,w1, w5, w3, w4,w5];
-
+const images = [w1, w5, w3, w4, w1, w5, w3, w4, w5];
 
 const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [itemsPerPage, setItemsPerPage] = useState(1);
+  const [touchStartX, setTouchStartX] = useState(0);
+  const [touchEndX, setTouchEndX] = useState(0);
 
   useEffect(() => {
     const updateItemsPerPage = () => {
@@ -41,23 +42,46 @@ const Carousel = () => {
     setCurrentIndex(newIndex);
   };
 
+  const handleTouchStart = (e) => {
+    setTouchStartX(e.touches[0].clientX);
+  };
+
+  const handleTouchEnd = (e) => {
+    setTouchEndX(e.changedTouches[0].clientX);
+    handleTouchMove();
+  };
+
+  const handleTouchMove = () => {
+    if (touchStartX > touchEndX + 50) {
+      // Swipe left
+      nextSlide();
+    } else if (touchStartX < touchEndX - 50) {
+      // Swipe right
+      prevSlide();
+    }
+  };
+
   return (
     <section className="bg-secondary py-8">
       <div className="container mx-auto">
         <div className="grid grid-cols-1 items-center justify-center text-center">
           <Image src={iconSvg} alt="Wedding" className="w-20 h-20 mx-auto" />
-          <h2 className= {`${cinzelDecorative.className} text-3xl md:text-4xl  text-white mb-8`} >NOSOTROS</h2>
+          <h2 className={`${cinzelDecorative.className} text-3xl md:text-4xl text-white mb-8`}>
+            NOSOTROS
+          </h2>
         </div>
         <div className="relative max-w-4xl mx-auto">
           <div className="overflow-hidden relative">
             <div
               className="whitespace-nowrap transition-transform duration-500"
               style={{ transform: `translateX(-${(currentIndex / itemsPerPage) * 100}%)` }}
+              onTouchStart={handleTouchStart}
+              onTouchEnd={handleTouchEnd}
             >
               {images.map((src, index) => (
                 <div
                   key={index}
-                  className={`inline-block p-4  ${itemsPerPage === 3 ? 'w-1/3 ' : 'w-full'}`}
+                  className={`inline-block p-4 ${itemsPerPage === 3 ? 'w-1/3' : 'w-full'}`}
                 >
                   <Image
                     src={src}
@@ -92,28 +116,26 @@ const Carousel = () => {
             </svg>
           </button>
 
-          
           {/* Right Arrow */}
           <button
-          onClick={nextSlide}
-          className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-secondary text-white rounded-full focus:outline-none shadow-lg hover:bg-primary transition duration-200 ease-in-out flex items-center justify-center w-8 h-8 hover:scale-110"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="w-4 h-4"
+            onClick={nextSlide}
+            className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-secondary text-white rounded-full focus:outline-none shadow-lg hover:bg-primary transition duration-200 ease-in-out flex items-center justify-center w-8 h-8 hover:scale-110"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M8.25 4.5l7.5 7.5-7.5 7.5"
-            />
-          </svg>
-        </button>
-
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="w-4 h-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </button>
         </div>
         <div className="flex justify-center space-x-2 mt-4">
           {Array(Math.ceil(images.length / itemsPerPage)).fill().map((_, index) => (
