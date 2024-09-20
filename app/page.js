349e9b811d1717f06b907dad580c4225753e05eb
BorisWaisten <boriswaisten@gmail.com>
2024-09-20 11:54:03 -0300

    .

--- app/page.js
+++ app/page.js
@@ -1,3 +1,29 @@
+import Header from '@/components/Header';
+import Gallery from '@/components/Gallery';
+import Invitation from '@/components/Invitation';
+import Footer from '@/components/Footer';
+import '../components/components.css';
+import Party from '@/components/Party';
+import Date from '@/components/Date';
+import Regalos from '@/components/Gifts';
+import ConfirmarAsistencia from '@/components/ConfirmAsistency';
+import { layourMetadata } from './layout';
+import MusicPlayer from '@/components/MusicPlayer'; // Importa el nuevo componente
+
+export const metadata = {
+  ...layourMetadata,
+  openGraph: {
+    images: [
+      {
+        url: '/martu1.jpg', // La ruta de la imagen en tu carpeta public
+        width: 1200,
+        height: 630,
+        alt: 'Imagen representativa de la boda de M&G',
+      },
+    ],
+  }
+}
+
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center">
@@ -21,9 +47,12 @@ const Home = () => {
         <section id="asistency">
           <ConfirmarAsistencia />
         </section>
+      
       </main>
       <Footer />
-      <MusicPlayer /> {/* Lo puedes dejar fuera del <main> también */}
+      <MusicPlayer />
     </div>
   );
 };
+
+export default Home;
