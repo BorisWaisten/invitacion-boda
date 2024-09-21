f5efadcdb9eabe4cb362de427e5cd3358aa1656f
BorisWaisten <boriswaisten@gmail.com>
2024-09-20 21:14:53 -0300

    .

--- components/MusicPlayer.jsx
+++ components/MusicPlayer.jsx
@@ -7,7 +7,7 @@ const MusicPlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const iframeRef = useRef(null);
 
-  // Al montar el componente, asegúrate de que el video esté en pausa
+  // Asegúrate de que el video esté en pausa al montar el componente
   useEffect(() => {
     const iframeWindow = iframeRef.current?.contentWindow;
     if (iframeWindow) {
@@ -19,6 +19,7 @@ const MusicPlayer = () => {
 
   const togglePlay = () => {
     const iframeWindow = iframeRef.current.contentWindow;
+
     if (isPlaying) {
       // Pausar la música de YouTube
       iframeWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
@@ -26,6 +27,7 @@ const MusicPlayer = () => {
       // Reanudar la música de YouTube
       iframeWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
     }
+    
     setIsPlaying(!isPlaying);
   };
 
@@ -38,7 +40,7 @@ const MusicPlayer = () => {
         height="0"
         src="https://www.youtube.com/embed/j09cI6t18NA?enablejsapi=1&autoplay=0&loop=1&playlist=j09cI6t18NA"
         style={{ position: 'absolute', width: '0px', height: '0px', border: 'none', overflow: 'hidden' }}
-        allow="autoplay; encrypted-media"
+        allow="autoplay; encrypted-media; fullscreen" // Ajuste importante para móviles
         allowFullScreen
         tabIndex="-1"
         aria-hidden="true"
@@ -46,11 +48,11 @@ const MusicPlayer = () => {
 
       {/* Botón con SVG animado */}
       <button
-        onClick={togglePlay} // Usa solo onClick para manejar tanto clic como touch
+        onClick={togglePlay}
         className={`relative p-1 bg-primary rounded-full transition-all duration-300 ${isPlaying ? 'animate-shadow-expand' : ''}`}
       >
         <img
-          src="/reproduccion.svg" // Asegúrate de que el SVG esté en la carpeta public
+          src="/reproduccion.svg"
           alt="Botón de música"
           className={`w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10 ${isPlaying ? 'animate-jump' : ''}`} // Animación de "saltitos"
         />
