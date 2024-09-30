472ea8d7cb67791147a0843890a679da25758011
BorisWaisten <boriswaisten@gmail.com>
2024-09-30 09:34:55 -0300

    version 2 music player

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ import Regalos from '@/components/Gifts';
 import ConfirmarAsistencia from '@/components/ConfirmAsistency';
 import { layourMetadata } from './layout';
 import MusicPlayer from '@/components/MusicPlayer'; // Importa el nuevo componente
+import MusicPlayer2 from '@/components/MusicPlay2';
 
 export const metadata = {
   ...layourMetadata,
@@ -49,7 +50,7 @@ const Home = () => {
         </section>
       </main>
       <Footer />
-      <MusicPlayer />
+      <MusicPlayer2 />
     </div>
   );
 };
