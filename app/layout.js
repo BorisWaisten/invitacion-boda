49fdbcbdea52cde596bd50a2e72e52df2f6bc9b4
BorisWaisten <boriswaisten@gmail.com>
2024-10-01 11:42:13 -0300

    .

--- app/layout.js
+++ app/layout.js
@@ -5,22 +5,28 @@ export const layourMetadata = {
   title: 'M&G - 22/03/2025',
   description: '¡Nos casamos! Ven y celebra con nosotros este día tan especial.',
   icons: {
-    icon:'/logo.ico',
+    icon: '/logo.ico',
   },
-  metadataBase: new URL('https://martuygino.vercel.app'), // Agrega esta línea
+  metadataBase: new URL('https://martuygino.vercel.app'),
   openGraph: {
     title: 'M&G - Nuestra Boda',
     description: '¡Nos casamos! Ven y celebra con nosotros este día tan especial.',
     url: 'https://martuygino.vercel.app/',
     type: 'website',
-    images: [{},],
+    images: [{}],
   },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className= {`text-primary ${montserrat.className}`} >{children}</body>
+    <html lang="es">
+      <head>
+        {/* Meta etiqueta para desactivar el zoom en móviles */}
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      </head>
+      <body className={`text-primary ${montserrat.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
