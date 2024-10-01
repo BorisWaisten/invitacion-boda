7dcc659072a8d6ececbe4bc18943b5afe849a7f3
BorisWaisten <boriswaisten@gmail.com>
2024-10-01 11:11:42 -0300

    .

--- components/Gifts/Gifts.jsx
+++ components/Gifts/Gifts.jsx
@@ -4,21 +4,21 @@ import { useRouter } from "next/navigation";
 export default function Regalos() {
     const router = useRouter();
     return (
-      <div className="bg-primary text-white text-center py-10">
-        <img className="h-[10vh] md:h-[15vh] lg:h-[20vh] mx-auto " src="/regalos.svg" alt="" />
-        <h2 className=" md:text-2xl lg:text-3xl font-bold mb-4">REGALOS</h2>
-        <div className="flex justify-center w-3/4 mx-auto">
-          <p className="mb-1  italic sm:text-sm md:text-base lg:text-lg">
+      <div className="bg-primary text-white text-center w-full py-10">
+        <img className="h-[12vh] md:h-[20vh] lg:h-[20vh] mx-auto " src="/regalos.svg" alt="" />
+        <h2 className=" text-2xl md:text-2xl lg:text-3xl font-bold mb-4">REGALOS</h2>
+        <div className="flex justify-center w-full ">
+          <p className=" italic text-sm sm:text-xl  lg:text-2xl">
             Tu presencia es lo más importante para nosotros.
           </p>
         </div>
-        <div className="flex justify-center w-3/4 mx-auto">
-        <p className="mb-4  italic sm:text-sm md:text-base lg:text-lg">
-            Si deseas hacernos un regalo, puedes ayudarnos con nuestra luna de miel.</p>
+        <div className="flex justify-center w-full">
+        <p className="mb-4  italic text-sm sm:text-xl lg:text-2xl">
+            Si deseas hacernos un regalo, será muy bienvenido.</p>
         </div>
         <button
             onClick={() => router.push('/gifts')}
-            className="md:text-2xl lg:text-3xl bg-secondary font-bold py-2 px-4 rounded-3xl">
+            className=" text-sm md:text-lg lg:text-xl bg-secondary font-bold py-2 px-4 rounded-3xl">
               MÁS INFORMACIÓN
         </button>
       </div>
--- components/Header.jsx
+++ components/Header.jsx
@@ -20,20 +20,27 @@ const Header = () => {
         {/* Capa de superposición con gradiente oscuro */}
         <div className="absolute "></div>
         <div className="absolute inset-0 flex flex-col justify-center items-center text-white">
-          <p className="text-lg md:text-2xl lg:text-3xl font-bold mb-4 underline">22.03.2025</p>
-          <div className="flex flex-col items-center">
+        <p className="flex items-center text-lg md:text-2xl lg:text-3xl font-bold mb-4">
+          <span className="flex-grow h-px w-[15vw] md:w-[15vw] lg:w-[15vw]   bg-white mr-10 md:mr-20 lg:mr-20"></span> {/* Línea antes del texto */}
+          22.03.2025
+          <span className="flex-grow h-px w-[15vw] md:w-[15vw] lg:w-[15vw]  bg-white ml-10 md:ml-20 lg:ml-20"></span> {/* Línea después del texto */}
+        </p>
+          <div className="flex flex-col items-center w-full">
             <h1 className={`titleHeader text-center ${cinzelDecorative.className}`}>
               Martu & Gino
             </h1>
-            {/* SVG ajustado para ser responsivo */}
-            <svg viewBox="0 0 400 50" width="100%" height="10vh" xmlns="http://www.w3.org/2000/svg">
+            <svg 
+              viewBox="0 0 400 50" 
+              className="w-[80%]  sm:w-[80%] md:w-[80%] lg:w-[60%]" 
+              preserveAspectRatio="xMidYMid meet" 
+              xmlns="http://www.w3.org/2000/svg">
               <circle cx="25" cy="25" r="5" fill="white" />
-              <line x1="30" y1="25" x2="370" y2="25" stroke="white" strokeWidth="3" />
+              <line x1="30" y1="25" x2="370" y2="25" stroke="white" strokeWidth="2" />
               <circle cx="375" cy="25" r="5" fill="white" />
             </svg>
           </div>
-          <p className="text-2xl sm:text-3xl md:text-2xl lg:text-5xl italic font-extrabold mt-4">¡NOS CASAMOS!</p>
-          <p className="font-bold text-lg md:text-lg lg:text-xl mt-4 md:mt-20 text-center">
+          <p className="text-2xl sm:text-3xl md:text-2xl lg:text-5xl italic mb-6 md:mb-0 font-extrabold ">¡NOS CASAMOS!</p>
+          <p className="font-bold text-sm md:text-lg lg:text-xl mt-4 md:mt-20 text-center">
             El amor no se deleita en la maldad, <br />
             sino que se regocija con la verdad. <br />
             Todo lo disculpa, todo lo cree, <br />
