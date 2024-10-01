b5f8964c0a3278739d6eb4cc8a1fdde984a8b193
BorisWaisten <boriswaisten@gmail.com>
2024-09-30 21:14:08 -0300

    .

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export const metadata = {
       {
         url: '/imagenPortada.jpg', // La ruta de la imagen en tu carpeta public
         width: 1200,
-        height: 630,
+        height: 800,
         alt: 'Imagen representativa de la boda de M&G',
       },
     ],
